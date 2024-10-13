3b9f01b5e3657193c27e2e9d675db9f7186722df
oyeyiolarebecca <oyeyiolarebecca16@gmail.com>
2024-10-13 23:50:44 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = set([0])  # Start with the first box unlocked
+    keys = set(boxes[0])  # Initially, we have keys from the first box
+    
+    while keys:
+        new_key = keys.pop()  # Take a key from the set
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)  # Unlock the corresponding box
+            keys.update(boxes[new_key])  # Add keys from the newly unlocked box
+    
+    # If the number of unlocked boxes equals the total number of boxes, return True
+    return len(unlocked) == n
